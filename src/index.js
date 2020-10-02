b433f9e3fb8097a58d2c8e71af80e385622b0e3c
Qairkhan <shyngys94@gmail.com>
2020-10-02 20:45:34 +0300

    fix: min , max function

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
+exports.min = function min(array) {
+    if (array == undefined || array.length == 0) {
+        return 0;
+    } else {
+        return Math.min(...array);
+    }
+};
 
-exports.min = function min (array) {
- 
-    return Math.min(...array);
+exports.max = function max(array) {
+    if (array == undefined || array.length == 0) {
+        return 0;
+    } else {
+        return Math.max(...array);
+    }
+};
 
-  
-}
-
-exports.max = function max (array) {
-
-    return Math.max(...array);
-}
-
-exports.avg = function avg (array) {
-  if (!array || !array.length) {
-    return 0
-} else {
-    const avg = array.reduce((a, b) => a + b, 0) / array.length;
-    return avg;
-}
-}
+exports.avg = function avg(array) {
+    if (!array || !array.length) {
+        return 0;
+    } else {
+        const avg = array.reduce((a, b) => a + b, 0) / array.length;
+        return avg;
+    }
+};
