42937746f317305f72f3ba60bc04af5ed91de71e
Qairkhan <shyngys94@gmail.com>
2020-10-02 20:29:20 +0300

    fix: fixed min and max function

--- src/index.js
+++ src/index.js
@@ -1,26 +1,21 @@
 
 exports.min = function min (array) {
-  if (!array || !array.lenght) {
-    return 0
-  } else {
+ 
     return Math.min(...array);
-  }
+
   
 }
 
 exports.max = function max (array) {
-  if (!array || !array.lenght) {
-    return 0
-  } else {
+
     return Math.max(...array);
-  }
 }
 
 exports.avg = function avg (array) {
   if (!array || !array.length) {
     return 0
 } else {
-    const avg = array.reduce((a, b) => a + b) / array.length;
+    const avg = array.reduce((a, b) => a + b, 0) / array.length;
     return avg;
 }
 }
